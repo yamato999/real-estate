41e6413e008a7303dd471f33adcb3001ea14ef81
Amir <amirbarkavi7@gmail.com>
2023-10-30 03:21:19 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Offers from "./pages/Offers";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
      <Router>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home/>}></Route>
         <Route path="/profile" element={<Profile/>}></Route>
