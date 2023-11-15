f8d60a75e681bdeff0cfabdff3b2ca43b3cb7a25
Amir <amirbarkavi7@gmail.com>
2023-11-15 23:23:39 +0600

    create listing UI created

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import PrivateRoute from "./components/PrivateRoute";
 import Header from "./components/Header";
+import CreateListing from "./pages/CreateListing";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -24,6 +25,7 @@ function App() {
           <Route path="/sign-up" element={<SignUp />}></Route>
           <Route path="/forgot-password" element={<ForgotPassword />}></Route>
           <Route path="/offers" element={<Offers />}></Route>
+          <Route path="/create-listing" element={<CreateListing />} />
         </Routes>
       </Router>
       <ToastContainer
